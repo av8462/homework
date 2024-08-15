d7f4e8bacb0fba52fb8d6a091654cbfc53cfb452
user <sichav@inbox.ru>
2024-08-15 16:41:55 +0300

    Homework 14.10.2024

--- task_3.js
+++ task_3.js
@@ -1,16 +1,42 @@
 `use  strict`;
 
-const scoreDolphins = (96 + 108 + 89) / 3;
-const scoreKoalas = (88 + 91 + 110) / 3;
-
-if (scoreDolphins > scoreKoalas) {
-  console.log("Dolphins win the trophy");
+function calcTip(bill) {
+  if (bill >= 50 && bill <= 300) {
+    const tip = bill * 0.15;
+    console.log(`The bill was ${bill}, the tip was ${tip}`);
+    return tip;
+  }
+  const tip = bill * 0.2;
+  console.log(`The bill was ${bill}, the tip was ${tip}`);
+  return tip;
 }
 
-if (scoreDolphins < scoreKoalas) {
-  console.log("Koalas win the trophy");
+function result(a, b) {
+  for (let i = 0; i < a.length; i++) {
+    a[i] = a[i] + b[i];
+  }
+  return a;
 }
 
-if (scoreDolphins === scoreKoalas) {
-  console.log("Both win the trophy");
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+
+const tips = bills.map(calcTip);
+
+const totals = result(bills, tips);
+
+console.log(`All tips from each bill :${tips}`);
+
+console.log(`All bill + tip from each bill :${totals}`);
+
+function calcAverage(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
+  return sum;
 }
+
+const sumTotals = calcAverage(totals);
+const averageTotals = sumTotals / totals.length;
+
+console.log(`Average total :${averageTotals}`);
